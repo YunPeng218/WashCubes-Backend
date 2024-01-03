c7e89328a67cd079fc2e9aaf11685accce64c0a3
Ivan Tan <ivantanyenwen02@gmail.com>
2024-01-03 18:19:54 +0800

    database connection setup

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+// REQUIRE ENVIRONMENT VARIABLES
+require('dotenv').config();
+
+// EXPRESS APP SETUP
 const PORT = 3000;
 const app = express();
 
+// DATABASE CONNECTION
+mongoose.connect(process.env.DB_CONNECT);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB Atlas connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB Atlas.');
+});
+
+// API ROUTES
 app.get('/', (req, res) => {
     res.send('Hello :)');
 });
 
+// SERVER LISTENING 
 app.listen(PORT, () => {
     console.log(`Server is runnning on Port ${PORT}`);
 })
+
